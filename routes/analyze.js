3c71475140cd58a1957c2039d03dd807387dd4f9
smitnikumbh <official.smit.niks@gmail.com>
2026-04-26 12:51:43 +0530

    production

--- routes/analyze.js
+++ routes/analyze.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-const admin = require("firebase-admin");
 
+require("dotenv").config();
 // Init Gemini
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
-// Init Firebase Admin
+
+const admin = require("firebase-admin");
+
 if (!admin.apps.length) {
-  const serviceAccount = require("../serviceAccountKey.json");
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
   });
 }
 
--- routes/recommend.js
+++ routes/recommend.js
@@ -2,13 +2,16 @@ const express = require("express");
 const router = express.Router();
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const admin = require("firebase-admin");
-
+require("dotenv").config();
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 if (!admin.apps.length) {
-  const serviceAccount = require("../serviceAccountKey.json");
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
   });
 }
 
